407940e0b70b3e17634296105f1f52d4946c01c9
dixita <ndixita@gmail.com>
2018-10-02 19:57:29 -0400

    minor modifications to trie
    Signed-off-by: dixita <ndixita@gmail.com>

--- ds/trie.cpp
+++ ds/trie.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 
 const int ALPHASIZE = 26;
+
 struct TrieNode {
     bool isEndOfWord;
-    TrieNode *children[ALPHASIZE];
+    TrieNode* children[ALPHASIZE];
 };
 
 TrieNode* getNewNode() {
@@ -15,15 +16,16 @@ TrieNode* getNewNode() {
     for(int i = 0; i < ALPHASIZE; i++) {
         p->children[i] = NULL;
     }
+
     return p;
 }
 
 void insert(TrieNode* root, string key) {
-    TrieNode *current = root;
+    TrieNode* current = root;
     for(int i = 0; i < key.length(); i++) {
         int index = key[i] - 'a';
         if(!current->children[index]) current->children[index] = getNewNode();
-        current = current->children[index];   
+        current = current->children[index];
     }
     current->isEndOfWord = true;
 }
@@ -36,17 +38,25 @@ bool search(TrieNode* root, string key) {
         current = current->children[index];
     }
 
-    return current != NULL && current->isEndOfWord;
+    return current && current->isEndOfWord;
 }
 
 int main() {
-    string keys[] = {"my", "name", "is", "dixita"};
-    TrieNode *root = getNewNode();
-    int N = sizeof(keys)/sizeof(keys[0]);
-    for(int i = 0; i < N; i++) {
-        insert(root, keys[i]);
+    string temp, toSearch;
+    cout << "Enter the words you want to insert" << endl;
+    TrieNode* root = getNewNode();
+    while((cin >> temp)) {
+        int n;
+        insert(root, temp);
+        cout << "Press 0 to break" << endl;
+        cin >> n;
+        if(n == 0) break;
     }
 
-    search(root, "dixita") ? cout << "Yes" : cout << "No";
+    cout << "Enter the word to be searched" << endl;
+    cin >> toSearch;
+    
+    search(root, toSearch) ? cout << "Found! :-D" << endl : cout << "Not Found :-(" << endl;
+
     return 0;
 }
